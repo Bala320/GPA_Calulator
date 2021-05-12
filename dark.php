57c8fe4a66bab564934d0ef48c67cc4c8085062b
Bala Subramaniyan <mspb.bala@gmail.com>
2021-05-12 11:01:12 +0530

    initial

--- dark.php
+++ dark.php
@@ -1,33 +1,11 @@
-<?php
-$themeClass = '';
-if (!empty($_COOKIE['theme'])) {
-  if ($_COOKIE['theme'] == 'dark') {
-    $themeClass = 'dark-theme';
-  } else if ($_COOKIE['theme'] == 'light') {
-    $themeClass = 'light-theme';
-  }  
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<!-- etc. -->
-<body class="<?php echo $themeClass; ?>">
-<!-- etc. -->
-<script>
-  const btn = document.querySelector(".btn-toggle");
-  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-  
-  btn.addEventListener("click", function() {
-    if (prefersDarkScheme.matches) {
-      document.body.classList.toggle("light-mode");
-      var theme = document.body.classList.contains("light-mode") ? "light" : "dark";
-    } else {
-      document.body.classList.toggle("dark-mode");
-      var theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
-    }
-    document.cookie = "theme=" + theme;
+jQuery(document).ready(function($) {
+  $(".toggle").click(function() {
+    $(".toggle").toggleClass("active");
+    $("body").toggleClass("night");
+    $.cookie("toggle", true);
   });
-</script>
-</body>
-</html>
+
+  if ($.cookie("toggle") == "true") {
+    $(".toggle").click();
+  }
+});
