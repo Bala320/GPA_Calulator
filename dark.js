94934eb5ed9981bd722cecc7e9a41b735f44027c
Bala Subramaniyan <mspb.bala@gmail.com>
2021-05-17 20:49:16 +0530

    initial

--- dark.js
+++ dark.js
@@ -1,11 +1,18 @@
-jQuery(document).ready(function($) {
-    $(".toggle").click(function() {
-      $(".toggle").toggleClass("active");
-      $("body").toggleClass("night");
-      $.cookie("toggle", true);
-    });
-  
-    if ($.cookie("toggle") == "true") {
-      $(".toggle").click();
-    }
-  });
+
+const options
+{
+  bottom: '64px', 
+  right: 'unset', 
+  left: '32px', 
+  time: '0.5s', 
+  mixColor: '#fff', 
+  backgroundColor: '#fff',  
+  buttonColorDark: '#100f2c', 
+  buttonColorLight: '#fff', 
+  saveInCookies: false,
+  label: '🌓';
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
